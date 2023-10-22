18ab7175111c507214ca57b18893026ab6522d3b
Agung Trisutaji Aprian <145095717+gagalsarjana@users.noreply.github.com>
2023-10-23 00:41:19 +0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -23,30 +23,17 @@ function anotherExample(firstLine, lastLine) {
 
 You can also make text **bold**... whoa!
 Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
+Or... **_both!_**
 
-There's also [links](https://www.freecodecamp.org), and
+There's also [links](https://www.freecodecamp.com), and
 > Block Quotes!
 
-And if you want to get really crazy, even tables:
-
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | -------------
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
+![React Logo w/ Text](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png)
 
 - And of course there are lists.
   - Some are bulleted.
-     - With different indentation levels.
+      - With different indentation levels.
         - That look like this.
-
-
-1. And there are numbered lists too.
-1. Use just 1s if you want!
-1. And last but not least, let's not forget embedded images:
-
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
 `;
 
 function App() {
@@ -54,21 +41,19 @@ function App() {
 
   return (
     <>
-      <body>
-        <div>
-          <h1 style={{textAlign: 'center'}}>Markdown Previewer</h1>
-          <div className='boxes-container'>
-            <textarea
-              name='editor'
-              id='editor'
-              value={markdownText}
-              onChange={(e) => setMarkdownText(e.target.value)}></textarea>
-            <div id='preview'>
-              <ReactMarkdown>{markdownText}</ReactMarkdown>
-            </div>
+      <div>
+        <h1 style={{textAlign: 'center'}}>Markdown Previewer</h1>
+        <div className='boxes-container'>
+          <textarea
+            name='editor'
+            id='editor'
+            value={markdownText}
+            onChange={(e) => setMarkdownText(e.target.value)}></textarea>
+          <div id='preview'>
+            <ReactMarkdown>{markdownText}</ReactMarkdown>
           </div>
         </div>
-      </body>
+      </div>
     </>
   );
 }
